1752ae26bc46b8dff783f34d3247dd844b0be728
csc-president <csc.president@uom.edu.gr>
2024-01-22 20:10:27 +0200

    Completion and further improvements in main.py. Smaller improvements in other files.Created test.py.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from os.path import join
  
  
 def read_json_file(file_path: str) -> Dict[str, Any]:
-    with open(file_path) as json_file:
+    with open(file_path, 'r', encoding='utf-8') as json_file:
         return json.load(json_file)
  
 class TokenEstimator:
@@ -20,7 +20,7 @@ class TokenEstimator:
     def estimate(self, text: str) -> int:
         return len(self.encoding.encode(text))
  
-class AnnotedSection:
+class AnnotatedSection:
     def __init__(
             self, 
             class_id: List[str], 
@@ -31,6 +31,9 @@ class AnnotedSection:
         self.text = text
         self.importance = importance
         self.class_name = class_name
+        
+    
+        
  
 class AnnotatedDecision:
     def __init__(
@@ -39,7 +42,7 @@ class AnnotatedDecision:
             court: str, 
             legal_remedy: str, 
             related_department: str,
-            annotations: List[AnnotedSection]) -> None:
+            annotations: List[AnnotatedSection]) -> None:
         
         self.document_id = document_id
         self.court = court
@@ -52,7 +55,7 @@ class AnnotatedDecision:
         annotated_decision_dict = read_json_file(annotated_decision_path)
  
         annotated_sections = [
-            AnnotedSection(
+            AnnotatedSection(
                 section['class_id'], 
                 section['text'], 
                 annotated_mappings_dict[section['class_id']]['importance'],
@@ -71,16 +74,12 @@ class AnnotatedDecision:
         document = ''
         for annotated_section in self.annotations:
             document = f'{document} {annotated_section.text}'
-            # print(annotated_section.class_id)
-            # print(annotated_section.text)
-            # print('\n\n\n\n\n\n\n\n\n\n')
  
         return document
     
     def get_document_tokens(self, token_estimator: TokenEstimator) -> int:
         return token_estimator.estimate(self.construct_document())
- 
- 
+    
     
 def split_to_sentences(text: str) -> List[str]:
     sentences = []
@@ -93,24 +92,49 @@ def split_to_sentences(text: str) -> List[str]:
     
     return sentences
  
-def openaicompletion(client: OpenAI, text):
+def openaicompletion(client: OpenAI, text, my_temperature, my_content, my_max=350):
     response = client.chat.completions.create(
         model='gpt-3.5-turbo',
+        temperature=my_temperature,
+        #max_tokens=my_max,
         messages=[
             {
                 'role': 'system',
-                'content': 'You are a helpful legal professional'
+                'content': 'You are a legal professional. You will create summaries of court decisions in Greek using legal language'
             },
             {
                 'role': 'user',
-                #'content': f'Summarize this:\n{text}'
-                'content': f'Summarize and translate to Greek:\n{text}'
+                'content': f'{my_content}:\n{text}'
+                
             }
         ],
     )
  
     return response
- 
+
+def create_chunks(sentences,token_estimator, max_chunk_token_size):
+    chunks = []
+    chunk = ''
+    chunk_token_size = 0
+    for sentence in sentences:
+        sentence_tokens = token_estimator.estimate(sentence)
+        if chunk_token_size + sentence_tokens < max_chunk_token_size:
+            chunk = f'{chunk} {sentence}'
+            chunk_token_size = chunk_token_size + sentence_tokens
+        else:
+            chunks.append(chunk)
+            chunk = sentence
+            chunk_token_size = sentence_tokens
+    return chunks
+    
+def summarize_summaries(chunks, token_estimator, section, client, my_temperature, my_content):
+    merged_section_summary = ''
+    for item in chunks:
+        item_max_tokens=max(token_estimator.estimate(item)*section.importance, 350)
+        response = openaicompletion(client, item, my_temperature, my_content, int(item_max_tokens))
+        summary = response.choices[0].message.content
+        merged_section_summary = f'{merged_section_summary} {summary}'
+    return merged_section_summary
  
 def main():
     load_dotenv()
@@ -127,63 +151,114 @@ def main():
     print(annotated_decision.document_id)
  
     client = OpenAI(api_key=OPENAI_KEY)
- 
-    text = "11. Επειδή, με την 686/2018 απόφαση της Ολομέλειας του Συμβουλίου της Επικρατείας κρίθηκε ότι από τον συνδυασμό των διατάξεων των άρθρων 4 παρ. 1, 5 παρ. 1 και 16 του Συντάγματος δεν αποκλείεται η πρόβλεψη από τον νομοθέτη ευνοϊκών ρυθμίσεων για τους αθλητές που έχουν επιτύχει εξαιρετικές αγωνιστικές διακρίσεις σε σημαντικές αθλητικές διοργανώσεις, όπως η θέσπιση ενός συστήματος πριμοδότησης βαθμολογίας για την εισαγωγή τους στην τριτοβάθμια εκπαίδευση, και μάλιστα, όχι μόνο σε συναφείς με τον αθλητισμό σχολές (π.χ. Τ.Ε.Φ.Α.Α.), αλλά και στα λοιπά Α.Ε.Ι. και Τ.Ε.Ι. της χώρας, προκειμένου οι υποψήφιοι αυτοί, λόγω της απαιτούμενης για την επίτευξη των ως άνω διακρίσεων έντονης ενασχολήσεώς τους με τον αθλητισμό, να μην βρεθούν σε ήσσονα μοίρα έναντι των λοιπών υποψηφίων, σχετικά με την επιλογή των σπουδών τους και τις βάσει αυτών περαιτέρω επαγγελματικές τους επιδιώξεις. Περαιτέρω έγινε δεκτό ότι ο καθορισμός των κατηγοριών διακεκριμένων αθλητών, στους οποίους παρέχεται το σχετικό ευεργέτημα ανήκει στην ευχέρεια του νομοθέτη και υπόκειται σε έλεγχο ορίων από τα δικαστήρια, όπως και οι θεσπιζόμενες σχετικές ευνοϊκές ρυθμίσεις, οι οποίες πρέπει, πάντως, να τελούν σε αρμονία προς την υποχρέωση του νομοθέτη να εξασφαλίζει την πρόσβαση στην τριτοβάθμια εκπαίδευση προσώπων κεκτημένων τα αναγκαία εφόδια για την ενεργό παρακολούθηση της θεωρητικής και πρακτικής διδασκαλίας των ανωτάτων εκπαιδευτικών ιδρυμάτων, ώστε να επιτυγχάνεται η αποστολή των ιδρυμάτων αυτών και η εύρυθμη λειτουργία τους. Ενόψει των ανωτέρω, με την ως άνω 686/2018 απόφαση της Ολομελείας κρίθηκε ότι η διάκριση που θεσπίζει η διάταξη της παρ. 8 του άρθρου 34 του ν. 2725/1999 υπέρ των αθλητών υποψηφίων που έχουν επιτύχει τις προβλεπόμενες από τον νόμο διακρίσεις (καθιέρωση συστήματος προσαύξησης της βαθμολογίας τους ποσοστιαίως, ανάλογα με την επιτευχθείσα αγωνιστική διάκριση), αναφερόμενη σε κατηγορίες υποψηφίων που προσδιορίζονται με αντικειμενικά κριτήρια, δικαιολογείται από λόγους δημοσίου συμφέροντος και, συγκεκριμένα, από την ανάγκη παροχής κινήτρων στους ασχολούμενους με τον αθλητισμό, για την ανάπτυξη του οποίου οφείλει να μεριμνά η Πολιτεία, κατ’ άρθρο 16 παρ. 9 του Συντάγματος και είναι, κατ’ αρχήν, συνταγματικώς θεμιτή, εφόσον, μάλιστα, με τη διάταξη αυτή δεν θίγεται ο αριθμός των κανονικώς εισακτέων στα τμήματα και τις σχολές των Α.Ε.Ι. και Τ.Ε.Ι., καθόσον το καθοριζόμενο από αυτήν ποσοστό υπέρ της ως άνω κατηγορίας υποψηφίων τίθεται καθ’ υπέρβαση του ολικού αριθμού εισακτέων για κάθε σχολή ή τμήμα σχολής των Α.Ε.Ι. και Τ.Ε.Ι. Περαιτέρω, όμως, κατά τα γενόμενα δεκτά με την ίδια ως άνω 686/2018 απόφαση του Συμβουλίου της Επικρατείας, οι εν λόγω διατάξεις του άρθρου 34 του ν. 2725/1999, όπως αυτό ίσχυε μετά την τροποποίησή του με το άρθρο 17 παρ. 1 του ν. 4429/2016, αντίκεινται στις συνταγματικές αρχές της ισότητας, της αξιοκρατίας και της ορθολογικής οργανώσεως της παρεχόμενης εκπαιδεύσεως, σε συνδυασμό με τη συνταγματική αρχή της αναλογικότητας μέτρου/σκοπού, κατά το μέρος που προβλέπουν: α) την πριμοδότηση αθλητών ακόμα και για επιτυχίες σε αθλητικούς αγώνες ήσσονος σημασίας, β) την καθ’ υπέρβαση εισαγωγή αθλητών στις ανώτατες σχολές στο δυσανάλογα υψηλό ποσοστό του 4,5% των προβλεφθεισών θέσεων, το οποίο παρίσταται αυθαίρετο ενόψει και του πάγιου χαρακτήρα της ρυθμίσεως αυτής, η οποία δεν προϋποθέτει οποιαδήποτε επίκαιρη εκτίμηση της δυνατότητας μιας σχολής για απορρόφηση σπουδαστών και γ) την εισαγωγή διακριθέντος αθλητή σε σχολή της προτιμήσεώς του, εφόσον συγκεντρώσει αριθμό μορίων τουλάχιστον ίσο με το 90% του αριθμού των μορίων του τελευταίου εισαχθέντος στη συγκεκριμένη σχολή κατά το ίδιο ακαδημαϊκό έτος, μετά την προσαύξηση του συνόλου των μορίων του από τις αθλητικές του διακρίσεις. Τούτο δε, διότι - όπως κρίθηκε - οι ανωτέρω ρυθμίσεις υπερακοντίζουν τον δημοσίου συμφέροντος σκοπό που υπηρετεί η διάταξη (προαγωγή του αθλητισμού), λόγω αφενός του επιβαλλόμενου με αυτές σημαντικού περιορισμού προσβάσεως στην τριτοβάθμια εκπαίδευση των λοιπών υποψηφίων φοιτητών που δεν είναι αθλητές και, αφετέρου, της σημαντικής υποχωρήσεως των ακαδημαϊκών κριτηρίων πρόσβασης, την οποία συνεπάγονται. Συνεπώς, το Δικαστήριο έκρινε με την απόφαση αυτή (686/2018) ότι η επίμαχη διάταξη της παρ. 8 του άρθρου 34 του ν. 2725/1999 παραβιάζει τα άρθρα 4 παρ. 1, 5 παρ. 1 και 16 του Συντάγματος, κατά το μέρος που οι εν λόγω ρυθμίσεις υπερβαίνουν, κατά τα ανωτέρω, τα ακραία όρια πέραν των οποίων το παρεχόμενο από τη διάταξη αυτή προνόμιο εμφανίζεται δικαιολογημένο και επιτρεπτό συνταγματικώς."
- 
     
     token_estimator = TokenEstimator()
  
     decision_tokens = annotated_decision.get_document_tokens(token_estimator)
-    print(decision_tokens)
  
-    summary_tokens = min(decision_tokens * 0.05, 11000)
-    print(summary_tokens)
+    summary_tokens = max(decision_tokens * 0.3, 15000)
  
     all_importance_times_tokens = sum([(annotation.importance * token_estimator.estimate(annotation.text)) for annotation in annotated_decision.annotations])
-    print(all_importance_times_tokens)
- 
+    print("all_importance_times_tokens=",all_importance_times_tokens)
+    law_list=[]
+    merged_full_summary=''
     for section in annotated_decision.annotations:
         print(section.class_name)
+        
+        if section.class_name=='other':
+            continue
+        elif section.class_name=='admissibility':
+            my_temperature=0.7
+            my_content="Summarize. If you can't, give the basic notion"
+        elif section.class_name=='overview':
+            my_temperature=0.8
+            my_content="Summarize. If you can't, give the basic notion"
+        elif section.class_name=='law':
+            my_temperature=1.0
+            my_content="Give the references of all the important laws (article/paragraphs included) in this text."
+        elif section.class_name=='interpretation':
+            my_temperature=0.5
+            my_content="Mention the law that is interpreted and summarize"
+        elif section.class_name=='previous-ruling':
+            my_temperature=0.7
+            my_content="Summarize. If you can't, give the basic notion"
+        elif section.class_name=='facts':
+            my_temperature=1.0
+            my_content="Summarize the key facts"
+        elif section.class_name=='party-claims':
+            my_temperature=0.5
+            my_content="Summarize. If you can't, give the basic notion"
+        elif section.class_name=='court-response':
+            my_temperature=0.5
+            my_content="Summarize. If you can't, give the basic notion"
+        elif section.class_name=='court-ruling':
+            my_temperature=1
+            my_content="Tell me what the court ruled"
+        elif section.class_name=='important':
+            my_temperature=0.5
+
         section_input_tokens = token_estimator.estimate(section.text)
         importance_times_tokens = section_input_tokens * section.importance
         portion = importance_times_tokens / all_importance_times_tokens
-        
-        section_summary_tokens = portion*summary_tokens
-        
-        if section_summary_tokens + section_input_tokens > OPENAI_TOKEN_LIMIT:
+        section_summary_tokens = max (portion*summary_tokens, 400)
+        print(section_summary_tokens)
+        if section_summary_tokens + section_input_tokens +60> OPENAI_TOKEN_LIMIT:
+            print(section.class_name, "was sliced")
             sentences = split_to_sentences(section.text)
-            
             n_chunks = math.ceil((section_summary_tokens + section_input_tokens) / OPENAI_TOKEN_LIMIT)
- 
-            max_chunk_token_size = (section_summary_tokens/ n_chunks) + (section_input_tokens / n_chunks)
- 
-            chunks = []
+            max_chunk_token_size = max((section_summary_tokens/ n_chunks) + (section_input_tokens / n_chunks)-60, 400)
+            chunks=create_chunks(sentences,token_estimator, max_chunk_token_size)
+            merged_section_summary=summarize_summaries(chunks, token_estimator, section, client, my_temperature, my_content)
+            print("if ", merged_section_summary)
+            # chunks = []
+            # chunk = ''
+            # chunk_token_size = 0
+            # for sentence in sentences:
+            #     sentence_tokens = token_estimator.estimate(sentence)
+            #     if chunk_token_size + sentence_tokens < max_chunk_token_size:
+            #         chunk = f'{chunk} {sentence}'
+            #         chunk_token_size = chunk_token_size + sentence_tokens
+            #     else:
+            #         chunks.append(chunk)
+            #         chunk = sentence
+            #         chunk_token_size = sentence_tokens
+            # merged_section_summary = ''
+            # for item in chunks:
+            #     item_max_tokens=max(token_estimator.estimate(item)*section.importance, 350)
+            #     response = openaicompletion(client, item, my_temperature, my_content, int(item_max_tokens))
+            #     summary = response.choices[0].message.content
+            #     merged_section_summary = f'{merged_section_summary} {summary}'
+            if section.class_name=="law":
+                law_list.append(merged_section_summary)
+                continue
+            merged_section_summary_tokens=token_estimator.estimate(merged_section_summary)
+            while merged_section_summary_tokens>3300:
+                sentences=split_to_sentences(merged_section_summary)
+                chunks=create_chunks(sentences,token_estimator, 3300)
+                merged_section_summary=summarize_summaries(chunks, token_estimator, section, client, my_temperature, my_content)
+                print("while ", merged_section_summary)
+                merged_section_summary_tokens=token_estimator.estimate(merged_section_summary)
+            #TO DO: what if merged_section_summaries is too long? Chunks, same as before 
+            # merged_section_summaries = openaicompletion(client, merged_section_summary, 1, "Summary this in Greek", int(section_summary_tokens))
+            # summary_of_summaries = merged_section_summaries.choices[0].message.content
+            # merged_full_summary=merged_full_summary+summary_of_summaries
+            print(merged_section_summary)
+            merged_full_summary=merged_full_summary+merged_section_summary
             
-            chunk = ''
-            chunk_token_size = 0
-            for sentence in sentences:
-                sentence_tokens = token_estimator.estimate(sentence)
-                if chunk_token_size + sentence_tokens < max_chunk_token_size:
-                    chunk = f'{chunk} {sentence}'
-                    chunk_token_size = chunk_token_size + sentence_tokens
-                else:
-                    chunks.append(chunk)
-                    chunk = sentence
-                    chunk_token_size = sentence_tokens
- 
- 
-            merged_summary = ''
-            for item in chunks:
-                response = openaicompletion(client, item)
-                summary = response.choices[0].message.content
- 
-                merged_summary = f'{merged_summary} {summary}'
-            print(merged_summary)
+        else:
+            response = openaicompletion(client, section.text, my_temperature, my_content, int(section_summary_tokens))
+            summary = response.choices[0].message.content
+            merged_full_summary=merged_full_summary+summary
+            print(merged_full_summary)
             
-            merged_summaries = openaicompletion(client, merged_summary)
-            summary_of_summaries = merged_summaries.choices[0].message.content
-            print(summary_of_summaries)
- 
+    print(annotated_decision.document_id)
+    print(annotated_decision.court)
+    print(annotated_decision.related_department)
+    print(annotated_decision.legal_remedy)
+    print("ΣΗΜΑΝΤΙΚΗ ΝΟΜΟΘΕΣΙΑ")
+    print(law_list)
+    print ("ΠΕΡΙΛΗΨΗ")
+    print(merged_full_summary)
             
             #print(chunks)
             # for item in chunks:
